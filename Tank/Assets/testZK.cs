393ba10624f6ef308eab187eb18678c65c4fe450
xxxxp <39648196@qq.com>
2018-06-27 22:30:45 +0800

    fist commit

---
+++ Tank/Assets/testZK.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class testZK : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("zk just do it");
+            GetComponent<Text>().text = "zk is a gua pi";
+            GetComponent<Text>().fontSize = 48;
+        }
+
+	}
+}
